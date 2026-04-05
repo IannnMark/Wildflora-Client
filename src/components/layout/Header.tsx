1a52fa906f2af7021acfeea3b96eda7079e52e53
Ian Mark <ianmarkmorga07@gmail.com>
2026-04-05 19:54:20 +0800

    Add hover styling on header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,7 +5,7 @@ import { Menu, Search, User } from "lucide-react";
 
 export default function Header() {
   return (
-    <div className="bg-[#FEFCEB] h-[57px] w-full">
+    <div className="bg-[#FEFCEB] h-[57px] sm:h-[104px] w-full">
       <div className="flex items-center justify-between px-8 md:px-16 h-full cursor-pointer">
         <Link href={"/"}>
           <Image
@@ -13,22 +13,32 @@ export default function Header() {
             alt="Wildflora"
             width={150}
             height={150}
-            className="h-[25px] w-[101px] sm:h-[30px] sm:w-[105px]"
+            className="h-[25px] w-[101px] sm:h-[40px] sm:w-[162px] hover:scale-105 transition-all duration-300"
           />
         </Link>
         <nav className="hidden lg:block w-[656px] h-[24px]">
           <ul className="flex justify-between gap-10 font-medium text-base font-satoshi text-[#1E1E1E]">
-            <li>Home</li>
-            <li>About</li>
-            <li>Journal</li>
-            <li>Plan Library</li>
-            <li>Community</li>
-            <li>Contact</li>
+            <li className="hover:scale-105 transition-all duration-300">
+              Home
+            </li>
+            <li className="hover:scale-105 transition-all duration-300">
+              About
+            </li>
+            <li className="hover:scale-105 transition-all duration-300">
+              Journal
+            </li>
+            <li className="transition-all duration-300">Plan Library</li>
+            <li className="hover:scale-105 transition-all duration-300">
+              Community
+            </li>
+            <li className="hover:scale-105 transition-all duration-300">
+              Contact
+            </li>
           </ul>
         </nav>
         <div className="flex items-center justify-between gap-5">
-          <Search className="h-[20px] w-[20px] hidden md:block" />
-          <User className="h-[20px] w-[20px] hidden md:block" />
+          <Search className="h-[20px] w-[20px] hidden md:block hover:scale-105 transition-all duration-300" />
+          <User className="h-[20px] w-[20px] hidden md:block hover:scale-105 transition-all duration-300" />
           <Menu className="h-[20px] w-[20px] lg:hidden" />
         </div>
       </div>
