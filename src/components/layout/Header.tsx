6dad027680c9bddd6389c1cbbec6ec77a823231a
Ian Mark <ianmarkmorga07@gmail.com>
2026-04-26 12:56:20 +0800

    Add hover styling on header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -21,13 +21,16 @@ export default function Header() {
             alt="Wildflora"
             width={150}
             height={150}
-            className="h-[25px] w-[101px] sm:h-[40px] sm:w-[162px] hover:scale-105 transition-all duration-300"
+            className="h-[25px] w-[101px] sm:h-[40px] sm:w-[162px] hover:scale-105 hover:brightness-0 hover:invert-[30%] hover:sepia-[100%] hover:saturate-[500%] hover:hue-rotate-[350deg] transition-all duration-300"
           />
         </Link>
         <nav className="hidden lg:block w-[656px] h-[24px]">
           <ul className="flex justify-between gap-10 font-medium text-base font-satoshi text-[#1E1E1E]">
             {navLinks.map((link) => (
-              <li key={link.name} className="relative group">
+              <li
+                key={link.name}
+                className="relative group hover:text-[#722F37]"
+              >
                 <Link href={link.href} className="pb-1">
                   {link.name}{" "}
                   <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-current transition-all duration-300 group-hover:w-full"></span>
@@ -37,9 +40,9 @@ export default function Header() {
           </ul>
         </nav>
         <div className="flex items-center justify-between gap-5">
-          <Search className="h-[20px] w-[20px] hidden md:block hover:scale-105 transition-all duration-300" />
-          <User className="h-[20px] w-[20px] hidden md:block hover:scale-105 transition-all duration-300" />
-          <Menu className="h-[20px] w-[20px] lg:hidden" />
+          <Search className="h-[20px] w-[20px] hidden md:block hover:scale-105 transition-all duration-300 hover:text-[#722F37]" />
+          <User className="h-[20px] w-[20px] hidden md:block hover:scale-105 transition-all duration-300 hover:text-[#722F37]" />
+          <Menu className="h-[20px] w-[20px] lg:hidden hover:text-[#722F37]" />
         </div>
       </div>
     </div>
