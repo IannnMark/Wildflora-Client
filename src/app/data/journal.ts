9b3615698676da5010a62e0c8a70f2e8417eb474
IanMark <ianmarkmorga07@gmail.com>
2026-09-07 15:44:12 +0800

    Add journal section

---
+++ src/app/data/journal.ts
@@ -0,0 +1,32 @@
+export const Journals = [
+  {
+    id: 1,
+    title: "5 Low-Maintenance Plants for Small Spaces",
+    image: "/images/journal/journal1.png",
+  },
+  {
+    id: 2,
+    title: "Soil & Repotting 101",
+    image: "/images/journal/journal2.png",
+  },
+  {
+    id: 3,
+    title: "Variegation Made Simple",
+    image: "/images/journal/journal3.png",
+  },
+  {
+    id: 4,
+    title: "5 Low-Maintenance Plants for Small Spaces",
+    image: "/images/journal/journal1copy.png",
+  },
+  {
+    id: 5,
+    title: "Soil & Repotting 101",
+    image: "/images/journal/journal2copy.png",
+  },
+  {
+    id: 6,
+    title: "Variegation Made Simple",
+    image: "/images/journal/journal3copy.png",
+  },
+];
