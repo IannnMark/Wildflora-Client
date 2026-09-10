14f281a0eb09a33d6730cb32583fadd6cd49f216
IanMark <ianmarkmorga07@gmail.com>
2026-09-10 13:41:14 +0800

    Add loading eager attribute

--- src/components/home/AboutSection.tsx
+++ src/components/home/AboutSection.tsx
@@ -23,6 +23,7 @@ export default function AboutSection() {
           alt=""
           width={1328}
           height={715}
+          loading="eager"
           sizes="(min-width: 1024px) 1328px, (min-width: 768px) 688px, 295px"
           className="h-[639px] w-[295px] rounded-[10px] object-cover md:h-[779px] md:w-[688px] lg:h-[715px] lg:w-[1328px] xl:h-[750px] xl:w-[1440px] 2xl:h-[900px] 2xl:w-[1800px]"
         />
